b8479ca38e9f79089cee9660b2107dedee39ab31
Benoit Garcia <benoitgar.pro@gmail.com>
2022-06-09 14:01:13 +0200

    ultimate last commit

--- src/components/TitleApp.js
+++ src/components/TitleApp.js
@@ -5,6 +5,6 @@ import {BsCart4} from 'react-icons/bs'
 
 export default function TitleApp() {
   return (
-    <h1 className="flex items-center text-cyan-600 text-2xl font-medium"><Link to="/"><BsCart4 className="mr-2" /> Bench<span className="font-bold">Marque</span></Link></h1>
+    <h1 className="flex items-center text-cyan-600 text-2xl font-medium"><Link to="/" className="flex items-center space-x-2"><BsCart4 className="mr-2" /> Bench<span className="font-bold">Marque</span></Link></h1>
   )
 }
