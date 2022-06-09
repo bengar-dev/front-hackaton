3aa081cbc066b864fdf88ef0de20eb49a81bc7bd
Benoit Garcia <benoitgar.pro@gmail.com>
2022-06-09 13:16:24 +0200

    fix: login

--- src/services/basicServices.js
+++ src/services/basicServices.js
@@ -14,7 +14,7 @@ export function errorTranslator(errorCode, errorContent) {
         }
 
         case "errLogin": return {
-            type: "errRegister",
+            type: "errLogin",
             msg: "Identifiant / Mdp Incorrect",
             statut: "error"
         }
